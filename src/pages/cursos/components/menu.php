3abc34facd434458ee4252682b2be69c7ddfb7dd
Rayan De Andrade Cata Preta <17500059647@tco.lan>
2024-10-30 08:18:13 -0300

    atualizar as estruturas de pastas

--- src/pages/cursos/components/menu.php
+++ src/pages/cursos/components/menu.php
@@ -7,19 +7,19 @@
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
+        <a href="./index.php" class="nav-link active" aria-current="page">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
           Cursos
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
+        <a href="./disciplinas.php" class="nav-link text-white">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
           Disciplinas
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
+        <a href="./matrizes_curriculares.php" class="nav-link text-white">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
           Matrizes curriculares.
         </a>
