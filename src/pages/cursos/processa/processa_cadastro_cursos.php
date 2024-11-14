fbafee519cef5084d25a216d2cfd53838710d467
Rayan De Andrade Cata Preta <17500059647@tco.lan>
2024-11-14 08:40:16 -0300

    atualizar o index de cursos

--- src/pages/cursos/processa/processa_cadastro_cursos.php
+++ src/pages/cursos/processa/processa_cadastro_cursos.php
@@ -3,16 +3,18 @@
     require_once("../../../adm/conexao.php");
 
     //preparar e executar o BD a inserção de dados\\
+    $nome = $_POST['nome'];
     $descricao = $_POST['descricao'];
     $status = $_POST['status'];
     $modalidade = $_POST['modalidade'];
 
 
     //criptografia
-    $sql = "INSERT INTO cursos (descricao, `status`, modalidade, created, modified) 
-                        VALUES (:descricao, :status, :modalidade, NOW(), NOW())";
+    $sql = "INSERT INTO cursos (nome, descricao, `status`, modalidade, created, modified) 
+                        VALUES (:nome, :descricao, :status, :modalidade, NOW(), NOW())";
 
     $comando = $pdo ->prepare($sql);
+    $comando -> bindParam(":nome", $nome);
     $comando -> bindParam(":descricao", $descricao);
     $comando -> bindParam(":status", $status);
     $comando -> bindParam(":modalidade", $modalidade);
